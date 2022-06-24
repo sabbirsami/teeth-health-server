921b69bfcfa6fbe52c0c81dbe92a44cea6219915
SabbirSami <smd71430@gmail.com>
2022-06-24 12:23:22 +0600

    stripe secret key added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ezzi1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 app.use(cors());
 app.use(express.json());
 
